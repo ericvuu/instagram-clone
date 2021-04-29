15b28ccd6d05630ab634b4bc43c1660d0fa9b304
Eric Vuu <ericlv91@gmail.com>
2021-04-29 11:57:11 -0700

    initial commit and clean up

---
+++ src/Components/Title.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Title = () => {
+  return (
+    <div>
+      <h1>InstaClone</h1>
+      <h2>My Inspirations</h2>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+    </div>
+  );
+}
+
+export default Title
